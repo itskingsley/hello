1d0754170a6d2c56d0f22a1f3bde727503b7764f
itskingsley <kingsleykwanga5@gmail.com>
2021-07-28 13:43:29 +0300

    first commit

---
+++ untitled1.py
@@ -0,0 +1,7 @@
+from tkinter import*
+root = TK()
+topframe = Frame(root)
+topframe.pack()
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+Button1 = Button(topframe, text =("Button1")="red")
